946e7108596ff0112f06f9d7d02436cbbb8b3385
Glauber Ramos <glauberamos@gmail.com>
2017-04-25 22:38:43 -0300

    More styling

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const map = { 0: 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Apr', 4: 'May', 5: 'Jun', 6: 'Jul', 7: 'Aug', 8: 'Sep', 9: 'Oct', 10: 'Nov', 11: 'Dec' }
+
+const doc = new jsPDF()
+let position = 15
+
 function drawHeader(position) {
   doc.setDrawColor(221, 221, 221)
   doc.setFillColor(245, 245, 245)
-  doc.rect(5, position, 150, 10, 'FD')
+  doc.roundedRect(5, position, 150, 12, 1, 1, 'FD')
 }
 
 function drawContent(position) {
@@ -10,28 +15,43 @@ function drawContent(position) {
   doc.rect(5, position, 150, 10, 'FD')
 }
 
-const map = { 0: 'Jan', 1: 'Feb', 2: 'Mar', 3: 'Apr', 4: 'May', 5: 'Jun', 6: 'Jul', 7: 'Aug', 8: 'Sep', 9: 'Oct', 10: 'Nov', 11: 'Dec' }
+function drawTitle() {
+  doc.setFontSize(22)
+  doc.setFontType("bold")
+  doc.text(data.name, 10, position)
+  position = position + 10
 
-const doc = new jsPDF()
-let position = 10
+  doc.setFontType("italic")
+  doc.setFontSize(14)
+  const date = new Date(data.updatedAt)
+  doc.text(date.getHours() + ':' + date.getMinutes() + ' ' + map[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear(), 10, position)
+  position = position + 5
+  doc.setFontType("normal")
 
-doc.text(data.name, 10, position)
-position = position + 10
+  doc.setLineWidth(.5)
+  doc.setDrawColor(230, 230, 230)
+  doc.line(10, position, 150, position)
+  position = position + 15
+  doc.setLineWidth(0)
+}
 
-const date = new Date(data.updatedAt)
-doc.text(date.getHours() + ':' + date.getMinutes() + ' ' + map[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear(), 10, position)
-position = position + 17
+function drawInfo() {
+  doc.text('Number of cards: ' + data.participants.length, 10, position)
+  position = position + 10
+  doc.text('Number of participants: ' + data.meta.cardCount, 10, position)
+  position = position + 10
+}
 
-doc.text('Number of cards: ' + data.participants.length, 10, position)
-position = position + 10
-doc.text('Number of participants: ' + data.meta.cardCount, 10, position)
-position = position + 10
+drawTitle();
+drawInfo();
 
 data.columns.map(function(column, index) {
   drawHeader(position)
 
   position = position + 7
+  doc.setFontSize(18)
   doc.text(column.placeholder, 10, position)
+  doc.setFontSize(14)
   position = position + 10
 
   column.rows.map(function(row, index) {
@@ -41,7 +61,7 @@ data.columns.map(function(column, index) {
 
     drawContent(position - 7)
 
-    doc.text('Votes ' + row.votes + ' - ' + row.value, 20, position)
+    doc.text('Votes ' + row.votes + ' - ' + row.value, 10, position)
     position = position + 10
 
     doc.setFontType("normal")
@@ -50,7 +70,7 @@ data.columns.map(function(column, index) {
       row.nodes.map(function(node, index) {
         drawContent(position - 7)
 
-        doc.text('Votes ' + node.votes + ' - ' + node.value, 20, position)
+        doc.text('Votes ' + node.votes + ' - ' + node.value, 10, position)
         position = position + 10
       })
     }
